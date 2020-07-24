1355a74bd9aecc79c4c2af0df9c7d6b2338f53f1
GemGar4 <gemma.garcias.e@gmail.com>
2020-07-24 02:54:52 +0200

    Rm jest preparation

--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const endpoint = process.env.API_ENDPOINT;
-
 const api = axios.create({
-   baseURL: endpoint || 'https://api.themoviedb.org/3/',
+   baseURL: 'https://api.themoviedb.org/3/',
    timeout: 15000
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
+      }
     ]
   },
   devServer: {
